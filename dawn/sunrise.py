ece5c7c73ab39f02752fbd075274df0f1616b680
Caleb Matthews <calebjmatthews@gmail.com>
2018-11-30 22:19:38 -0600

    Individual LED speed multipliers

--- dawn/sunrise.py
+++ dawn/sunrise.py
@@ -1,5 +1,6 @@
 import time
 import math
+import random
 from bootstrap import *
 
 TARGET_COLORS = [
@@ -22,8 +23,14 @@ class Sunrise:
     def __init__(self, sunriseDuration):
         self.sunriseDuration = sunriseDuration
         self.timeElapsed = 0
-        print('self.sunriseDuration')
-        print(self.sunriseDuration)
+        self.speedMults = self.genSpeedMults()
+        print('self.speedMults')
+        print(self.speedMults)
+
+    def genSpeedMults(self):
+        speedMults = []
+        for index in range(LED_COUNT):
+            speedMults.append(random.uniform(0.75, 1.0))
 
     def test(self):
         self.start()
@@ -33,18 +40,20 @@ class Sunrise:
     def start(self):
         led = LEDStrip(LED_COUNT)
         for colorIndex in range(len(TARGET_COLORS)-1):
-            self.transitionBetweenColors(TARGET_COLORS[colorIndex],
-                TARGET_COLORS[colorIndex+1],
-                self.sunriseDuration/len(TARGET_COLORS))
+            for ledIndex in range(LED_COUNT):
+                self.transitionBetweenColors(TARGET_COLORS[colorIndex],
+                    TARGET_COLORS[colorIndex+1],
+                    self.sunriseDuration/len(TARGET_COLORS), ledIndex)
 
     def end(self):
         led = LEDStrip(LED_COUNT)
         for colorIndex in reversed(range(1, len(TARGET_COLORS))):
-            self.transitionBetweenColors(TARGET_COLORS[colorIndex],
-                TARGET_COLORS[colorIndex-1], 0.1)
+            for ledIndex in range(LED_COUNT):
+                self.transitionBetweenColors(TARGET_COLORS[colorIndex],
+                    TARGET_COLORS[colorIndex-1], 0.1, ledIndex)
         led.all_off()
 
-    def transitionBetweenColors(self, color1, color2, duration):
+    def transitionBetweenColors(self, color1, color2, duration, ledIndex):
         ticksPassed = 0
         tickLength = MIN_TICK_LENGTH
         ticksTotal = math.floor(duration / MIN_TICK_LENGTH)
@@ -54,9 +63,10 @@ class Sunrise:
         for tickIndex in range(ticksTotal):
             ticksPassed += 1
             self.timeElapsed += tickLength
-            progress = ticksPassed / ticksTotal
+            speedMult = self.speedMults[ledIndex]
+            progress = ticksPassed / (ticksTotal * speedMult)
             colorSet = self.genColorSet(color1, color2, progress)
-            led.fillRGB(colorSet[0], colorSet[1], colorSet[2])
+            led.setRGB(ledIndex, colorSet[0], colorSet[1], colorSet[2])
             led.update()
             print('colorSet')
             print(colorSet)
